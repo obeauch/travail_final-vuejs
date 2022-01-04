a0e1a9f88586ca1924ac7075ace3290af5c0ad19
obeauch <obeauch@gmail.com>
2022-01-04 14:59:46 -0500

    avec_volume

--- js/components/player.js
+++ js/components/player.js
@@ -9,10 +9,13 @@ export default externalTemplate({
             chansons: [],
             texte_input: "",
             chanson_selection: false,
-
             audio: new Audio(),
             musique_joue: false,
             temps_actuel: 0,
+            son: 0.5,
+            show_volume: "",
+            src: "songs/",
+            current_src: "",
         }
     },
 
@@ -26,29 +29,52 @@ export default externalTemplate({
             })
         })
 
-
-        // play, pause, ended, timeupdate
-        this.audio.addEventListener("play", e => {
-            this.musique_joue = true
-        })
-
-        this.audio.addEventListener("timeupdate", e => {
-            this.temps_actuel = this.audio.currentTime
-        })
-
-        // <img :src="'images/' + jeu.images[0]" alt="">
-        this.audio.setAttribute("src", "sons/food-vlog-11204.mp3")
-        this.audio.play()
-        this.audio.volume = 0.1
-        // this.audio.pause()
-
-
     },
 
     methods: {
-        jouerChanson(chanson) {
+        afficherChanson(chanson) {
             this.chanson_selection = chanson
 
+            this.audio.addEventListener("timeupdate", e => {
+                this.temps_actuel = this.audio.currentTime
+            })
+
+
+            /**
+             * Lorsque une chanson est affichée, musique_joue est à false afin que le bouton play soit visible.
+             */
+            this.musique_joue = false
+
+
+
+            /**
+             * place dans la source audio le current_src
+             * qui contient src-> "songs/" et la chanson en lecture (chanson.audio)
+             **/
+            this.current_src = this.src + chanson.audio
+            this.audio.setAttribute("src", this.current_src)
+
+        },
+
+        // au click sur bouton play, la musique débute et musique_joue devient false, ce qui affiche bouton pause et vice/versa.
+        play() {
+            this.audio.addEventListener("play", e => {
+                this.musique_joue = true
+            })
+            this.audio.play()
+
+        },
+
+        pause() {
+            this.audio.addEventListener("pause", e => {
+                this.musique_joue = false
+            })
+            this.audio.pause()
+        },
+
+        volume() {
+            this.audio.volume = this.son
+            this.show_volume = "Volume (" + Math.round(this.son * 100) + "%)"
         }
 
     },
